b665e34c5bcc302445e5eaa6b826562926837fd4
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2025-06-07 19:06:08 +0400

    First commit

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <main>
+            <h1>This is Home Page</h1>
+        </main>
+     );
+}
+ 
+export default Home;
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <header>
+            <nav>
+                <p>Home</p>
+                <p>About Us</p>
+                <p>Services</p>
+            </nav>
+        </header>
+     );
+}
+ 
+export default Navbar;
