3ab73466e059895391cc80d5b3352a076ba09fe8
Luka Guledani <lussskki@gmail.com>
2025-06-10 17:15:08 +0400

    initial commit, register/login

---
+++ Back-end/Models/user-schema.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function (v) {
+                return /^.+@.+\..+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    rememberMe: {
+        type: Boolean,
+        default: false
+
+    },
+    cardInfo: {
+        cardNumber: String,
+        expiryDate: String,
+        cvv: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+export const User = mongoose.model("Users", userSchema)
+export default User
---
+++ Back-end/app.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+
+import { registerRouter } from './Routes/auth-route.js'
+import { loginRouter } from './Routes/auth-route.js'
+// import { createDiscountRouter } from './Routes/discount-route.js'
+// import { getDiscountRouter } from './Routes/discount-route.js'
+// import { updateDiscountRouter } from './Routes/discount-route.js'
+// import { deleteDiscountRouter } from './Routes/discount-route.js'
+
+// partner
+// import partnersRouter  from './Routes/partners-route.js'
+
+dotenv.config()
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const corsOptions = {
+    origin: "*", 
+    credentials: true, 
+} 
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muavqao.mongodb.net/`
+
+app.use('/register', registerRouter)
+app.use('/login', loginRouter)
+// app.use('/discount', createDiscountRouter)
+// app.use('/discounts', getDiscountRouter)
+// app.use('/discount', updateDiscountRouter)
+// app.use('/discount', deleteDiscountRouter)
+// app.use('/partner', partnersRouter)
+
+
+
+mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB connected"))
+    .catch((error) => {
+        console.log("Error", error.message)
+        process.exit(1)
+    })
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+// export default app
