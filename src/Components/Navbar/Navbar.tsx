3df64eddd25fab92cc0060fd82068bd92bc0ffe7
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2025-06-22 13:13:53 +0400

    small changes in feature1

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,7 @@
 import "./css/navbar.css";
-import Menu from "../../assets/icon-hamburger.svg";
-import { useState, useRef } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 import type { navigation } from "./type";
-import { Link } from "react-router-dom";
-import Close from "../../assets/icon-close.svg";
+// import hamburgerIcon from "../../assets/icon-hamburger.svg";
+import EasyWayLogo from "../../assets/easyWayLogo.jpeg";
 
 const navigation: navigation[] = [
   {
@@ -26,99 +23,20 @@ const navigation: navigation[] = [
 ];
 
 const Navbar = () => {
-  const [displayMenu, setDisplayMenu] = useState<boolean>(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-
   return (
     <>
       <header>
-        {displayMenu ? (
-          <AnimatePresence>
-            <motion.img
-              src={Close}
-              initial={{ opacity: 0, rotate: 0 }}
-              animate={{ opacity: 1, rotate: 90 }}
-              exit={{ opacity: 0, rotate: 0 }}
-              transition={{
-                type: "spring",
-                stiffness: 400,
-                damping: 13,
-                duration: 0.2,
-              }}
-              className="close-button"
-              alt="close icon"
-              onClick={() => setDisplayMenu(false)}
-            />
-          </AnimatePresence>
-        ) : (
-          <AnimatePresence>
-            <motion.img
-              className="menu-button"
-              src={Menu}
-              alt="menu icon"
-              onClick={() => setDisplayMenu(true)}
-            />
-          </AnimatePresence>
-        )}
-
-        <nav className="desktop-nav">
-          <ul className="desktop-routes-list">
-            {navigation.map((item, index) => {
-              return (
-                <li key={index} className="desktop-routes">
-                  <Link style={{ all: "unset" }} to={item.route}>
-                    {item.nav}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+        <nav>
+          <div className="logo-image-container">
+            <img width={"50px"} height={"50px"} src={EasyWayLogo} alt="" />
+          </div>
+          <div className="navItems-container">
+            {navigation.map((navItems, index) => (
+              <p key={index}>{navItems.nav}</p>
+            ))}
+          </div>
         </nav>
       </header>
-      <AnimatePresence mode="popLayout">
-        {displayMenu && (
-          <div className="menu">
-            {/* Overlay */}
-            <motion.div
-              className="menu-overlay"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 0.5 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.15 }}
-              onClick={() => setDisplayMenu(false)}
-            />
-            <motion.div
-              className="hamburger-menu"
-              ref={menuRef}
-              initial={{ opacity: 1, y: -300 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -100 }}
-              transition={{ duration: 0.15 }}
-            >
-              <motion.nav
-                initial={{ opacity: 1, y: -100 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -100 }}
-                transition={{ duration: 0.2 }}
-              >
-                <ul className="routes-list">
-                  {navigation.map((item, index) => (
-                    <li
-                      key={index}
-                      className="routes"
-                      onClick={() => setDisplayMenu(false)}
-                    >
-                      <Link style={{ all: "unset" }} to={item.route}>
-                        {item.nav}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </motion.nav>
-            </motion.div>
-          </div>
-        )}
-      </AnimatePresence>
     </>
   );
 };
