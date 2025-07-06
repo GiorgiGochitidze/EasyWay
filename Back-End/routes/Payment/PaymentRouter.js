17e29bc75efda96952116ffb577f06cd7e9fcf2a
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2025-07-06 21:46:57 +0400

    added missing import

--- Back-End/routes/Payment/PaymentRouter.js
+++ Back-End/routes/Payment/PaymentRouter.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const {
   createPaymentOrder,
+  handleBogCallback
 } = require("../../controllers/PaymentController/PaymentController");
 
 router.post("/create-order", createPaymentOrder);
-router.post("/callback", paymentController.handleBogCallback);
+router.post("/callback", handleBogCallback);
 
 module.exports = router;
